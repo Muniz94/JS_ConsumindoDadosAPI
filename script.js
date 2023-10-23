81b59d555e78d07859f14da1934fbf8a86cf4245
muniz94 <muniz91@hotmail.com>
2023-10-23 13:45:53 -0300

    Usei o método 'fetch(URL da API 'ViaCEP' no caso)' guardei numa variável e imprimi no console. O resultado foi uma PROMISE

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-// Síncrono
+var consultaCEP = fetch('viacep.com.br/ws/01001000/json/');
 
-console.log('Mandando oi pro amigo!');
-
-function mandaMensagem() {
-  console.log('Tudo bem?');
-  console.log('Vou te mandar uma solicitação!');
-  console.log('Solicitação recebida!');
-}
-
-mandaMensagem();
-
-console.log('Tchau, Tchau!');
-
-// Assíncrono : o setTimeOut faz com que a linha 25 seja executada por último,
-// chama a função após 5000ms no caso (5 segundos)
-
-console.log('Mandando oi pro amigo!');
-
-function mandaMensagem() {
-  console.log('Tudo bem?');
-  console.log('Vou te mandar uma solicitação!');
-  console.log('Solicitação recebida!');
-}
-
-setTimeout(mandaMensagem, 5000); // isso é um callback (basicamente uma função que é passada como argumento para outra função)
-console.log('Tchau, Tchau!');
+console.log(consultaCEP);
