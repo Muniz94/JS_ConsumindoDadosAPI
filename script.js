09a8ccf781e60e446f5db91850fd8e52445e1295
Lucas Muniz <145066504+Muniz94@users.noreply.github.com>
2023-10-24 11:48:56 -0300

    Adicionei um .finally

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
   .then(resposta => resposta.json())
   .then(r => {
     if (r.erro) {
-      throw Error('Esse CEP não existe!');
+      throw Error('Esse CEP não existe!'); // CEP válido e inexistente
     } else console.log(r);
+    // CEP válido e existente
   })
-  .catch(erro => console.log(erro));
+  .catch(erro => console.log(erro)) // CEP inválido
+  .finally(mensagem => console.log('Processamento concluído!'));
 
-console.log(consultaCEP);
+  // console.log(consultaCEP); valor da promise
