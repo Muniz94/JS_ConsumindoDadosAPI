5c38e5a0741117f273af8af62363036468bf19c1
Lucas Muniz <145066504+Muniz94@users.noreply.github.com>
2023-10-24 12:01:10 -0300

    Criei uma 'async' function juntamente com o 'await fetch' e converti com o '.json()'.

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-  .then(resposta => resposta.json())
-  .then(r => {
-    if (r.erro) {
-      throw Error('Esse CEP não existe!'); // CEP válido e inexistente
-    } else console.log(r);
-    // CEP válido e existente
-  })
-  .catch(erro => console.log(erro)) // CEP inválido
-  .finally(mensagem => console.log('Processamento concluído!'));
+async function buscaEndereco() {
+  var consultaCEP = await fetch('https://viacep.com.br/ws/01001000/json/');
+  var consultaCEPConvertida = await consultaCEP.json();
+  console.log(consultaCEPConvertida);
+}
 
-  // console.log(consultaCEP); valor da promise
+buscaEndereco();
